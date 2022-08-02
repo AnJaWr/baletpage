2ec045430224795dbebd99fb475e0e6991d45234
anjawr <48285666+AnJaWr@users.noreply.github.com>
2022-08-02 11:34:46 +0200

    laptop+mobile

--- js/main.js
+++ js/main.js
@@ -13,11 +13,16 @@ const kadra_btn = document.getElementById("kadra_btn");
 // const links = document.querySelectorAll(".page-header ul a");
 const kids_btn= document.getElementById("kids_btn");
 const adult_btn= document.getElementById("adult_btn");
-
+const balet_btn=document.getElementById("balet-btn")
+const barre_btn=document.getElementById("barre-btn")
+const stretching_btn=document.getElementById("stretching_btn")
 
 kadra_btn.addEventListener("click", clickHandler);
 adult_btn.addEventListener("click", clickHandler);
 kids_btn.addEventListener("click", clickHandler);
+barre_btn.addEventListener("click", clickHandler);
+balet_btn.addEventListener("click", clickHandler);
+stretching_btn.addEventListener("click", clickHandler);
 
 function clickHandler(e) {
   e.preventDefault();
